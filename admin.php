6d917e4955fe17e3deb70b8e6410164ce8eccf56
Nikita <no00499@rvt.lv>
2024-06-17 17:15:36 +0300

    jj

--- admin.php
+++ admin.php
@@ -3,6 +3,32 @@
 // jābūt iespējai ielogoties kā admins
 
 // jābūt iespējai pievienot bloga ierakstu blog.csv failā
+<?php
+	$adminUsername = "admin";
+	$adminPassword = 123;
 
-$adminUsername = "admin";
-$adminPassword = 123;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include "inc/head.php"; ?>
+<body>
+    <?php include "inc/navigation.php";?>
+	 <h2>Admin Login</h2>
+    <form action="admin.php" method="post">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+        </div>
+        <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+    
+</body>
+</html>
